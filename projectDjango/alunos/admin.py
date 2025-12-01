6ebba503de99985c8968566056a20d19be1cfc32
Diego Carvalho <diegoricardo2527@gmail.com>
2025-12-01 14:48:54 -0300

    feat: implementa migrações e superuser para modelo Aluno

--- projectDjango/alunos/admin.py
+++ projectDjango/alunos/admin.py
@@ -3,7 +3,7 @@ from .models import Aluno
 
 @admin.register(Aluno)
 class AlunoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email', 'cpf', 'data_ingresso')  # Removi data_criacao
+    list_display = ('nome', 'email', 'cpf', 'data_ingresso')
     list_display_links = ('nome', 'email')
     search_fields = ('nome', 'email', 'cpf')
     list_filter = ('data_ingresso',)
