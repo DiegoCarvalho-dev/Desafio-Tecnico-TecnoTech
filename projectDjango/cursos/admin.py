e66c260dfbc9c2216bb96e41a25e9ae60b97c25a
Diego Carvalho <diegoricardo2527@gmail.com>
2025-12-01 15:16:02 -0300

    feat: cria modelo Curso com status ativo/inativo

--- projectDjango/cursos/admin.py
+++ projectDjango/cursos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from alunos.models import Aluno
+from .models import Curso
+
+
+@admin.register(Curso)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'carga_horaria', 'valor_inscricao', 'status', 'data_criacao')
+
+    list_filter = ('status',)
+    search_fields = ('nome',)
+    list_per_page = 20
+    ordering = ('nome',)
--- projectDjango/cursos/models.py
+++ projectDjango/cursos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Curso(models.Model):
+    STATUS_CHOICES = (
+        ('ativo', 'Ativo'),
+        ('inativo', 'Inativo'),
+    )
+
+    nome = models.CharField(max_length=200)
+
+    carga_horaria = models.IntegerField(help_text='Carga horária em horas')
+
+    valor_inscricao = models.DecimalField(max_digits=10, decimal_places=2)
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='ativo')
+
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.nome} ({self.get_status_display()})'
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
