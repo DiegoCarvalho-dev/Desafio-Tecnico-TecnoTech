6ecbed9a2769747b1d41525323451295ccac76fe
Diego Carvalho <diegoricardo2527@gmail.com>
2025-12-01 13:29:22 -0300

    feat: cria modelo Aluno conforme foi especificado

--- projectDjango/alunos/admin.py
+++ projectDjango/alunos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Aluno
 
-# Register your models here.
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf', 'data_ingresso')  # Removi data_criacao
+    list_display_links = ('nome', 'email')
+    search_fields = ('nome', 'email', 'cpf')
+    list_filter = ('data_ingresso',)
+    list_per_page = 20
--- projectDjango/alunos/models.py
+++ projectDjango/alunos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    cpf = models.CharField(max_length=14, unique=True)
+    data_ingresso = models.DateField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Aluno'
+        verbose_name_plural = 'Alunos'
