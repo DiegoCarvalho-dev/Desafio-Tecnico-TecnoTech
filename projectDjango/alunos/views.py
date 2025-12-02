8c9e21bbc5aba580562ae3f44208634227209945
Diego Carvalho <diegoricardo2527@gmail.com>
2025-12-02 00:06:36 -0300

    Adiciona views para dashboard e histórico do aluno

--- projectDjango/alunos/views.py
+++ projectDjango/alunos/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Aluno
+from .serializers import AlunoSerializer
 
 
-@login_required
-def historico_aluno(request, aluno_id):
-    aluno = get_object_or_404(Aluno, id=aluno_id)
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    context = {
-        'aluno': aluno,
-        'matriculas': aluno.matriculas.all(),
-        'total_pago': aluno.total_pago,
-        'total_devido': aluno.total_devido,
-        'transacoes': aluno.historico_transacoes,
-    }
-
-    return render(request, 'alunos/historico.html', context)
+    @action(detail=True, methods=['get'])
+    def historico(self, request, pk=None):
+        aluno = self.get_object()
+        data = {
+            'aluno': AlunoSerializer(aluno).data,
+            'matriculas': aluno.matriculas.count(),
+            'total_pago': aluno.total_pago,
+            'total_devido': aluno.total_devido,
+            'matriculas_pagas': aluno.matriculas.filter(status_pagamento='pago').count(),
+            'matriculas_pendentes': aluno.matriculas.filter(status_pagamento='pendente').count(),
+        }
+        return Response(data)
